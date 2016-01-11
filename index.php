67bdc2d84ee21f45dece1dec7211cf1b60f58aa4
roydl7 <roydl.net@gmail.com>
2016-01-11 23:19:34 +0530

    Changed Paths

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
-
-<link rel="stylesheet" href="style/stylesheet.css"/>
-<script src="scripts/core.js"/>
+<link rel="stylesheet" href="css/style.css"/>
+<script src="js/core.js"/>
 
 
 <canvas id="game" width="800" height="400" style="margin:auto; border:1px solid #000000;">
