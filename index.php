7b341daf4a413b2902db2c724ac23af9cd358104
roydl7 <roydl.net@gmail.com>
2016-01-24 23:41:17 +0530

    Changed Paths

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
 <head>
-<link rel="stylesheet" href="css/style.css"></link>
+<link rel="stylesheet" href="game/css/style.css"></link>
 <script src="js/jquery-2.1.4.min.js"></script>
-<script src="js/core.js"></script>
-<script src="js/interface.js"></script>
+<script src="game/js/core.js"></script>
+<script src="game/js/interface.js"></script>
 
 
 </head>
 <body>
-<canvas id="game" width="100%" height="100%"></canvas>
+<canvas id="game"></canvas>
 <div id="cv-footer">
 	<div id="mutebtn" class="cv-footer-btn unmute"></div>
 	<div class="cv-footer-btn"></div>
